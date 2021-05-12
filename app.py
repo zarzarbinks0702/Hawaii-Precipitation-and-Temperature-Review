7c3c9a17687ac47fd83ce5ad45f93f4035746619
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-05-12 16:51:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 #import flask dependencies
 from flask import Flask, jsonify
 import csv
+import datetime as dt
+import pandas as pd
+
+#create the precipitation df
+precipitation_csv = 'Resources/hawaii_measurements.csv'
+precipitation_df = pd.read_csv(precipitation_csv, encoding='ISO-8859-1')
+
+#create the stations df
+stations_csv = 'Resources/hawaii_stations.csv'
+stations_df = pd.read_csv(stations_csv, encoding='ISO-8859-1')
+
+#create the merged df
+merged_df = pd.merge(precipitation_df, stations_df, on='station', how='outer')
 
 #flask setup
 app = Flask(__name__)
@@ -9,45 +22,55 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return (
-        f'Welcome to the Hawaii Rainfall and Temperature API!<br/>'
-        f'Available Routes:<br/>'
-        f'To see pairs of dates and precipitation measurements in inches for various stations in Hawaii: /api/v1.0/precipitation<br/>'
-        f'/api/v1.0/stations<br/>'
-        f'/api/v1.0/tobs<br/>'
-        f'//api/v1.0/<start><br/>'
+        f'Welcome to the Hawaii Rainfall and Temperature API!<br/><br/>'
+        f'Available Routes:<br/><br/>'
+        f'To see pairs of dates and precipitation measurements in inches for various stations in Hawaii:<br/>'
+        f'/api/v1.0/precipitation<br/><br/>'
+        f'To see a list of the stations used for measurements:<br/>'
+        f'/api/v1.0/stations<br/><br/>'
+        f'To see temperature observations in degrees F for the Waihee station:<br/>'
+        f'/api/v1.0/tobs<br/><br/>'
+        f'/api/v1.0/<start><br/>'
         f'/api/v1.0/<start>/<end><br/>'
     )
 
 #create precipitaion route
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    with open('Resources/hawaii_measurements.csv') as csv_file_1:
-        precipitation_csv = csv.reader(csv_file_1, delimiter=',')
+    prcp_dict = {}
 
-        prcp_dict = {}
+    for index, row in precipitation_df.iterrows():
+        prcp_dict[row['date']] = row['prcp']
 
-        for row in precipitation_csv:
-            prcp_dict[row[1]] = row[2]
-
-        return jsonify(prcp_dict)
+    return jsonify(prcp_dict)
     
 
 #create stations route
 @app.route('/api/v1.0/stations')
 def stations():
-    with open('Resources/hawaii_stations.csv') as csv_file_2:
-        stations_csv = csv.reader(csv_file_2, delimiter=',')
-        stations = []
+    stations = []
 
-        for row in stations_csv:
-            stations.append(row[1])
+    for index, row in stations_df.iterrows():
+        stations.append(row['name'])
 
-        return jsonify(stations)
+    return jsonify(stations)
 
 
 #create tobs route
-#@app.route('/api/v1.0/tobs')
-
+@app.route('/api/v1.0/tobs')
+def tobs():
+    one_year_ago = pd.to_datetime(dt.date(2017, 8, 23) - dt.timedelta(days=365))
+    
+    tobs_df = merged_df[['date', 'tobs', 'name']]
+    last_12_months_tobs_df = tobs_df.loc[(pd.to_datetime(tobs_df['date'], format='%Y-%m-%d')) > one_year_ago]
+    waihee_12mo_tobs_df = last_12_months_tobs_df.groupby('name').get_group('WAIHEE 837.5, HI US')
+    
+    waihee_data = {}
+    
+    for index, row in waihee_12mo_tobs_df.iterrows():
+        waihee_data[row['date']] = row['tobs']
+        
+    return jsonify(waihee_data)
 
 #create start route
 #@app.route('/api/v1.0/<start>')
